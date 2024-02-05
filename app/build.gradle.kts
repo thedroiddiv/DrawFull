f1af143a8607924279f6c85376dd6b265d4cc525
Divyansh Kushwaha <thedroiddiv@gmail.com>
2024-02-05 14:50:47 +0530

    build: use version catalogue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,9 +50,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
 
     val composeBom = platform(libs.compose.bom)
     implementation(composeBom)
